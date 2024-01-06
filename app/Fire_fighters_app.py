71f70f7cad8f3f8153983a1475e691f5adc7756b
J-Rahlf <148797759+J-Rahlf@users.noreply.github.com>
2024-01-06 10:20:55 +0100

    update ohne joblib

--- app/Fire_fighters_app.py
+++ app/Fire_fighters_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # import seaborn as sns
-import joblib
+# import joblib
 
 # Read data
 # df = pd.read_csv("oct23_bds_int_firefighters\app\test_sample_i.csv")
